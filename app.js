6478f7e6e5cacacf0a7ea87d1b8d6527b978a33a
Jordan <jojohersh@gmail.com>
2020-04-29 18:55:52 -0400

    added comments to db setup as well as routes for displaying them. Prettied up pages with bootstrap.

--- app.js
+++ app.js
@@ -2,38 +2,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const bodyParser = require('body-parser');
+const Campground = require("./models/campground");
+const Comment = require('./models/comment');
+seedDB = require("./seeds");
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/yelp_camp");
 
+seedDB();
+app.use(express.static(__dirname + "/public"))
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-//schema setup
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-//   {
-//     name: "Camp Aurora",
-//     image: "https://cdn.pixabay.com/photo/2016/02/09/16/35/night-1189929_960_720.jpg",
-//     description: "Roast marshmallows, play the guitar, brave the fury of mother nature, all under the ominous green of the Aurora Borrealis"
-//   }, (err, campground) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("Newly created campground:");
-//       console.log(campground);
-//     }
-//   }
-// );
-
+//INDEX ROUTES
 app.get("/", (req,res)=>{
   res.render("landing");
 });
@@ -43,13 +24,15 @@ app.get("/campgrounds", (req,res) => {
     if (err) {
       console.log(err);
     } else {
-      res.render("index",{campgrounds: allCampgrounds});
+      res.render("campgrounds/index",{campgrounds: allCampgrounds});
     }
   });
 });
+//NEW ROUTE
 app.get("/campgrounds/new", (req,res) => {
-  res.render("new");
+  res.render("campgrounds/new");
 });
+//CREATE ROUTE
 app.post("/campgrounds", (req,res) => {
   //get data from form and add to campgrounds array
   var name = req.body.name;
@@ -69,16 +52,52 @@ app.post("/campgrounds", (req,res) => {
 //SHOW - shows more info about one campground
 app.get("/campgrounds/:id", (req,res) => {
   // find campground with provided id through Mongoose
-  Campground.findById(req.params.id ,(err,foundCampground)=>{
+  Campground.findById(req.params.id).populate("comments").exec( (err,foundCampground)=>{
     if(err) {
       console.log(err);
     } else {
+      console.log(foundCampground);
       // render show template with that campground
-      res.render("show",{campground: foundCampground});
+      res.render("campgrounds/show",{campground: foundCampground});
     }
   });
 
 });
+// ------------------------------------------
+// COMMENTS ROUTES
+app.get("/campgrounds/:id/comments/new",(req,res)=>{
+  Campground.findById(req.params.id, (err,foundCampground)=>{
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("comments/new", {campground:foundCampground});
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", (req,res)=>{
+  //lookup campground by id
+  Campground.findById(req.params.id, (err,campground)=>{
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      //create new comment
+      Comment.create(req.body.comment,(err,comment)=>{
+        if(err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect(`/campgrounds/${campground._id}`);
+        }
+      });
+      //connect new comment to campground
+      //redirect to campground show page
+    }
+  })
+});
+// ------------------------------------------
 
 app.listen(3000, () =>{
   console.log("Yelp camp listening on port 3000...");
